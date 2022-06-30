9d178ca100ea3a02de1edc15fc9c440462d8ff4b
Gilnek <48454242+Gilnek@users.noreply.github.com>
2022-06-30 15:58:51 -0300

    Update teste.py

--- adpcounter/teste.py
+++ adpcounter/teste.py
@@ -23,11 +23,14 @@ cv.imwrite("dilation1.jpg", dilation)
 
 #aqui faz a comparaçao entre elas
 
+resultimage = np.zeros((2040, 1536))
 #feita a operacao de uma erosao-dilatacao 
 new = (erosion - dilation)*4
-#new = cv.bilateralFilter(new,9,75,75)
+#new = cv.normalize(((erosion - dilation)), resultimage, 50, 0, cv.NORM_MINMAX)
+#new = cv.normalize(new, resultimage, 50, 125, cv.NORM_MINMAX)
 #feita a operacao de uma dilatacao-erosao
 new2 = (dilation - erosion)*4
+#new2 = cv.normalize(new2, resultimage, 50, 125, cv.NORM_MINMAX)
 
 # new  = erosion-dilation
 # new2 = dilation-erosion
@@ -48,7 +51,8 @@ new_translation = cv.warpAffine(new, T, (width, height))
 #cv.imshow("Originalimage", new2)
 #cv.imshow('Translation', new2_translation)
 
-new4 = (new_translation + new)*2
+new4 = (new_translation + new)
+new4 = cv.normalize(new4, resultimage, 75, 150, cv.NORM_MINMAX)
 cv.imwrite("new4.jpg", new4)
 
 # Store height and width of the image
@@ -59,15 +63,16 @@ new2_translation = cv.warpAffine(new2, T, (width, height))
 #cv.imshow("Originalimage", new2)
 #cv.imshow('Translation', new2_translation)
 
-new3 = (new2_translation + new2)*2
+new3 = (new2_translation + new2)
+new3 = cv.normalize(new3, resultimage, 75, 150, cv.NORM_MINMAX)
 cv.imwrite("new3.jpg", new3)
 
 ksize = 5,5
-dilero = cv.blur(new3, ksize)*2
+dilero = cv.blur(new3, ksize)
 cv.imwrite("dileroblur.jpg", dilero)
 
 ksize = 5,5
-erodil = cv.blur(new4, ksize)*2
+erodil = cv.blur(new4, ksize)
 cv.imwrite("erodilblur.jpg", erodil)
 
 medianerodil = cv.bilateralFilter(new3, 9, 75, 75)
@@ -106,9 +111,9 @@ cv.imwrite("erosion2.jpg", erosion)
 mask = erosion
 cv.imwrite("mask.jpg", mask)
 
-kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3,3))
-opening = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel, iterations=2)
-close = cv.morphologyEx(opening, cv.MORPH_CLOSE, kernel, iterations=2)
+kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (7,7))
+opening = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel, iterations=1)
+close = cv.morphologyEx(opening, cv.MORPH_CLOSE, kernel, iterations=1)
 cv.imwrite("close.jpg", close)
 
 cnts = cv.findContours(close, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
